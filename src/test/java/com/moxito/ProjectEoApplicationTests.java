e4f2a6dc4891dbe58d30e43225a82dadee9a9862
mohit.kumar <mohit.kumar@magicbricks.com>
2022-06-05 21:26:16 +0530

    Prj-81 started

---
+++ src/test/java/com/moxito/ProjectEoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moxito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
